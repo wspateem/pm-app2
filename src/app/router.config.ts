3a954e5c3474b3cc97cc94de8c8d16651d741901
biernackiadam <biernackiadam@go2.pl>
2018-06-06 10:48:53 +0200

    Add files via upload

--- src/app/router.config.ts
+++ src/app/router.config.ts
@@ -1,23 +1,23 @@
 
 
-import {Route} from "@angular/router";
-import {HomeComponent} from "./home/home.component";
-import {LoginComponent} from "./login/login.component";
-import {OnLoginComponent} from "./on-login/on-login.component";
-import {RegisterComponent} from "./register/register.component";
-import {AuthGuard} from "./security/auth.guard";
-import { ResetComponent } from "./reset/reset.component";
-import { PersonsComponent } from "./persons/persons.component";
+import {Route} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
+import {AuthGuard} from './security/auth.guard';
+import { ResetComponent } from './reset/reset.component';
+import {PersonResolver} from './persons/person.resolver';
+import { PersonsListComponent } from './persons/persons-list/persons-list.component';
+import { NewPersonComponent } from './persons/new-person/new-person.component';
+import { PersonDetailComponent } from './persons/person-detail/person-detail.component';
+import { Component } from '@angular/core';
+import { PersonSearchComponent } from './persons/person-search/person-search.component';
+import { PersonFormComponent } from './persons/person-form/person-form.component';
 
-export const routerConfig : Route[] = [
+export const routerConfig: Route[] = [
     {
-        path:'home',
+        path: 'home',
         component: HomeComponent
-    },
-    {
-        path:'on-login',
-        component: OnLoginComponent,
-        canActivate: [AuthGuard]
     },
     {
         path: 'login',
@@ -29,8 +29,45 @@ export const routerConfig : Route[] = [
         canActivate: [AuthGuard]
     },
     {
-        path: 'persons',
-        component: PersonsComponent,
+        path: 'person-search/:id',
+        children: [
+           
+            {
+                path: '',
+                component: PersonDetailComponent,
+                canActivate: [AuthGuard]
+            },
+            {
+                path: ':id2',
+                children: [
+
+                    {
+                        path: '',
+                        component: PersonFormComponent,
+                        canActivate: [AuthGuard],
+                        resolve: {
+                            person: PersonResolver
+                          }
+                    }
+                ]
+            }
+           
+        ]
+      },
+      {
+          path: 'persons',
+          component: PersonsListComponent,
+          canActivate: [AuthGuard]
+      },
+
+    {
+        path: 'new',
+        component: NewPersonComponent,
+        canActivate: [AuthGuard]
+    },
+    {
+        path: 'view',
+        component: PersonDetailComponent,
         canActivate: [AuthGuard]
     },
     {
@@ -38,6 +75,17 @@ export const routerConfig : Route[] = [
         component: ResetComponent,
         canActivate: [AuthGuard]
     },
+    {
+        path: 'person-search',
+        component: PersonSearchComponent,
+        canActivate: [AuthGuard]
+    },
+    { path: 'person-form',
+    component: PersonFormComponent,
+    canActivate: [AuthGuard]
+
+    },
+
     {
         path: '',
         redirectTo: 'home',
